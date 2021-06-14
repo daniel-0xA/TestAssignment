761a782f2e7f6df50a4a3cd39a1df409adfec035
Daniel Sporis <dani@sporis.info>
2021-06-14 13:06:19 +0200

    small change

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run  -it \
+docker run  --rm \
     -v /home/dani/projects/testAssignment/Images:/usr/src/TestAssignment/Images \
     test_assignment \
     /usr/src/TestAssignment/TestAssignment -d Images
